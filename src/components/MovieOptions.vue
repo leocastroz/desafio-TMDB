f88ecef5a396fe5d3cf90fff7609c874a03f5a3d
leocastroz <castroz.dev@gmail.com>
2023-10-04 14:18:02 -0300

    fix: adjustment code

--- src/components/MovieOptions.vue
+++ src/components/MovieOptions.vue
@@ -1,29 +1,30 @@
 <template>
-    <div>
-      <select class="p-2 mx-2 rounded bg-gray-500 text-gray-300 cursor-pointer" :value="selectedOption" @change="onOptionChange">
-        <option value="now_playing">Recentes</option>
-        <option value="popular">Populares</option>
-        <option value="top_rated">Mais Votados</option>
-        <option value="upcoming">Em breve</option>
-      </select>
-    </div>
-  </template>
+  <div>
+    <select class="p-2 mx-2 rounded bg-gray-500 text-gray-300 cursor-pointer" :value="selectedOption"
+      @change="onOptionChange">
+      <option value="now_playing">Recentes</option>
+      <option value="popular">Populares</option>
+      <option value="top_rated">Mais Votados</option>
+      <option value="upcoming">Em breve</option>
+    </select>
+  </div>
+</template>
   
-  <script>
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
-    props: {
-      selectedOption: String,
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    selectedOption: String,
+  },
+  emits: ['update:selectedOption', 'fetchMovies'],
+  methods: {
+    onOptionChange(event) {
+      const selectedValue = event.target.value;
+      this.$emit('update:selectedOption', selectedValue);
+      this.$emit('fetchMovies');
     },
-    emits: ['update:selectedOption', 'fetchMovies'],
-    methods: {
-      onOptionChange(event) {
-        const selectedValue = event.target.value;
-        this.$emit('update:selectedOption', selectedValue); // Emita o evento personalizado para atualizar a prop
-        this.$emit('fetchMovies'); // Emita o evento personalizado para buscar filmes
-      },
-    },
-  });
-  </script>
+  },
+});
+</script>
   
