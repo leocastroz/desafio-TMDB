3c3fbc4f85470419b3b49e29d2da05be4ba872b6
leocastroz <castroz.dev@gmail.com>
2023-10-03 23:19:01 -0300

    refact: adjusting parent component

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -2,7 +2,11 @@
 </script>
 
 <template>
-  <main>
-    
-  </main>
+  <div class="w-2/5 mx-auto my-10 p-5">
+    <h1 class="text-xl font-bold">👋 Bem-vindo ao desafio Vue3 </h1>
+    <p class="my-5 text-justify tracking-wide leading-6 text-[13px]">Este desafio tem por finalidade testar o desenvolvedor com lógicas de programação,
+      analisando as habilidades do desenvolvedor com chamadas API, organização de código,
+      estruturação, sintaxe, interpretação de documentação, estilização, entre outros.
+    </p>
+  </div>
 </template>
